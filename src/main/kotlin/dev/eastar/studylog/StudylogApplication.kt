0e8b321c9636e1a4d9288e184051b6125e1d92ac
eastarj <eastarj@gmail.com>
2020-04-14 23:02:26 +0900

    first upload

---
+++ src/main/kotlin/dev/eastar/studylog/StudylogApplication.kt
@@ -0,0 +1,11 @@
+package dev.eastar.studylog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudylogApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudylogApplication>(*args)
+}
---
+++ src/test/kotlin/dev/eastar/studylog/StudylogApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.eastar.studylog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudylogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
