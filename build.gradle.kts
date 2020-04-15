db5e4ebd9d907917f8d11a877dee45bf47bc6897
eastarj <eastarj@gmail.com>
2020-04-16 06:09:25 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,24 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-batch")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
-	testImplementation("org.springframework.batch:spring-batch-test")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+	//implementation("org.springframework.boot:spring-boot-starter-batch")
+	//testImplementation("org.springframework.batch:spring-batch-test")
+
+ 	//testImplementation("com.h2database:h2")
+	//implementation("org.postgresql:postgresql")
+
 }
 
 tasks.withType<Test> {
