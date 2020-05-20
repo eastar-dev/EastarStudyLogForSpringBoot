55c8d51099087bd691a64e2366cbc0a28330b5a3
eastarj <eastarj@gmail.com>
2020-05-21 03:42:53 +0900

    .

--- src/main/kotlin/dev/eastar/studylog/item/ItemController.kt
+++ src/main/kotlin/dev/eastar/studylog/item/ItemController.kt
@@ -1,6 +1,7 @@
 package dev.eastar.studylog.item
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -11,32 +12,26 @@ interface StudyItemRepository : MongoRepository<StudyItem, String>
 @RestController
 @RequestMapping("/item")
 class ContentController {
-
     @Autowired
     lateinit var repository: StudyItemRepository
 
     @RequestMapping(method = [RequestMethod.GET, RequestMethod.POST], path = ["/{id}"])
-    fun post(@PathVariable(value = "id") id: Long): String = "post item $id"
+    fun post(@PathVariable(value = "id") id: String) = repository.findById(id)
 
     @RequestMapping(method = [RequestMethod.GET, RequestMethod.POST])
-    fun post(): String {
-        val items = repository.findAll()
-        items.forEach {
-            println(it.toString())
-        }
-        return items.toString()
-    }
+    fun post() = repository.findAll(Sort.by(Sort.Direction.DESC, "millisecond"))
 
     @PutMapping(consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     @ResponseBody
-    fun put(studyItem: StudyItem): String {
-        repository.insert(studyItem)
-        return studyItem.toString()
-    }
+    fun insert(studyItem: StudyItem) = repository.insert(studyItem)
+
+    //@PutMapping(consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    //@ResponseBody
+    //fun update(studyItem: StudyItem) = repository.findById(studyItem._id!!).up
 
     @DeleteMapping(path = ["/{id}"])
-    fun delete(@PathVariable(value = "id") id: Long): String = "delete item $id"
+    fun delete(@PathVariable(value = "id") id: String) = repository.deleteById(id)
 
     @DeleteMapping(path = ["/all"])
-    fun delete(): String = "delete item all"
+    fun delete() = repository.deleteAll()
 }
--- src/main/kotlin/dev/eastar/studylog/item/StudyItem.kt
+++ src/main/kotlin/dev/eastar/studylog/item/StudyItem.kt
@@ -1,11 +1,8 @@
 package dev.eastar.studylog.item
 
-data class StudyItem(var title: String, var content: String) {
-    private lateinit var _id: String
-    override fun toString(): String {
-        return "$_id : $title : $content"
-    }
-}
+import org.springframework.data.mongodb.core.aggregation.DateOperators
+
+data class StudyItem(var _id: String?, var nick: String = "noname", var title: String, var content: String, var millisecond: Long = System.currentTimeMillis())
 
 
 
